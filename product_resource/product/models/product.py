d43ed699f950b6fbb2eca6623a242c6f4d0eeef8
Bakhitarose <Bakhita.Okong'o@strathmore.edu>
2024-11-29 01:38:02 +0300

    create the product model

---
+++ product_resource/product/models/product.py
@@ -0,0 +1,6 @@
+
+class Product:
+    def __init__(self, name: str, description: str, price: float) -> None:
+        self.name = name
+        self.description = description
+        self.price = price
